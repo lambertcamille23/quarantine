676431564dbefb5f21563abf9c122f17de03e2e8
Camille Lambert <lambertcamille23@gmail.com>
2020-05-13 17:58:13 +0200

    Controller Category

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,6 +2,7 @@
 # The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
 #
 # Examples:
+categories = Category.create([{ name: 'Sport' }, { name: 'Bien-être' }, { name: 'Talk' }])
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
