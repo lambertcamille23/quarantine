c396223c7591dec2e628c3c4ac08309cc8f56327
camille lambert <lambertcamille23@gmail.com>
2020-05-28 11:47:40 +0200

    details

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,15 +2,17 @@
 # The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
 #
 # Examples:
+
+
 Category.destroy_all
 
-Category.create([{ name: 'YOGA / PILATE' }, { name: 'SPORT' }, { name: 'LIFESTYLE' },{ name: 'CULTURE' }, { name: 'FOOD' }, { name: 'KIDS / FAMILLE' }, { name: 'BIEN-ÊTRE / BEAUTÉ' }])
+Category.create([{ name: 'YOGA / PILATES', color: '#EDAF50' }, { name: 'SPORT', color: '#C35B37' }, { name: 'LIFESTYLE', color: '#EDCDBC' },{ name: 'CULTURE', color: '#F39B8F' }, { name: 'FOOD', color: '#065A69' }, { name: 'KIDS / FAMILLE', color: '#EDAF50' }, { name: 'BIEN-ÊTRE / BEAUTÉ', color: '#C35B37' }])
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-Category.all.each do |category|
-  3.times do
-    fake_descriptions = ["venez faire du sport avec une bli", "c'est l'heure de faire des trucs"]
-    Event.create(description: fake_descriptions.sample, link: "@quarantine", title:"YOGA", category: category, date: Time.zone.now - (1..60).to_a.sample.send(['hours', 'minutes', 'days'].sample))
-  end
-end
+#Category.all.each do |category|
+  #3.times do
+    #fake_descriptions = ["venez faire du sport avec une bli", "c'est l'heure de faire des trucs"]
+   #Event.create(description: fake_descriptions.sample, link: "@quarantine", title:"YOGA", category: category, date: Time.zone.now - (1..60).to_a.sample.send(['hours', 'minutes', 'days'].sample))
+  #end
+#end
